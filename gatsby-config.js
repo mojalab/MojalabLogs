5d6ae39b3df44b792c4910d52410d9181fc3d6ba
chisatoDream <chisato.lyrical@gmail.com>
2019-06-19 02:08:18 +0900

    update:もじゃブログ#1

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,22 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+  resolve: `gatsby-transformer-remark`,
+  options: {
+    plugins: [
+      {
+        resolve: `gatsby-remark-images`,
+        options: {
+          // It's important to specify the maxWidth (in pixels) of
+          // the content container as this plugin uses this as the
+          // base for generating different widths of each image.
+          maxWidth: 2000,
+        },
+      },
+    ],
+  },
+},
     'gatsby-transformer-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
